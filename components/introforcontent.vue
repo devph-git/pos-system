151c396f6d2bbfcc01f03558c83484debe94cfa6
Gil <quickerz59@gmail.com>
2020-07-14 23:18:29 +0800

    initial commit

---
+++ components/introforcontent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <dashboardIntroImage />
+  </div>
+</template>
+
+<script>
+import dashboardIntroImage from './dashboardIntroImage'
+
+export default {
+  name: 'contentIntro',
+  components: {
+    dashboardIntroImage
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
