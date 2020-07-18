83bc2e63c13e3eb2a56d14cad6db7df6bc9754d8
Adean <adean@anymindgroup.com>
2020-07-18 23:50:43 +0700

    41906445: use jwt token on swagger

--- api/config/l5-swagger.php
+++ api/config/l5-swagger.php
@@ -154,19 +154,11 @@ return [
         */
           
         //   Open API 3.0 support
-         'passport' => [ // Unique name of security
-            'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-            'description' => 'Laravel passport oauth2 security.',
-            'in' => 'header',
-            'scheme' => 'https',
-            'flows' => [
-                "password" => [
-                    "authorizationUrl" => config('app.url') . '/oauth/authorize',
-                    "tokenUrl" => config('app.url') . '/oauth/token',
-                    "refreshUrl" => config('app.url') . '/token/refresh',
-                    "scopes" => []
-                ],
-            ],
+        'passport' => [
+            'type' => 'http',
+            'scheme' => 'bearer',
+            'bearerFormat' => 'JWT',
+            'description' => 'Laravel with JWT authentication token.',
         ],
     ],
 
